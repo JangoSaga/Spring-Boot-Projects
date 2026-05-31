c206d055d04e9ca02ff3b71b3e911fd869f8c351
KARAN <vishwakarmakaran45@gmail.com>
2026-05-31 11:35:49 +0530

    Delete the task app moving on to the different project

--- taskapp/src/main/java/taskapp/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package taskapp.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import taskapp.Entitiy.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
